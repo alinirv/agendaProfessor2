ea3877e9a8d3285acd019beabaa6e31cacf28194
arv <alini.vicente@yahoo.com.br>
2024-08-20 20:10:35 -0300

    feat: add component footer

---
+++ src/components/Footer.js
@@ -0,0 +1,9 @@
+import React from 'react';
+
+function Footer(){
+    return(
+        <footer>
+            <p>&copy; {new Date().getFullYear()} Arvic. Todos diretos reservados.</p>
+        </footer>
+    )
+}
